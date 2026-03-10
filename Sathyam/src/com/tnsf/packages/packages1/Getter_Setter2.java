3bd2452f99421dd8c28ef8fe3c8a82f403e177b1
itharaju sathyam <itharajusatjhyam45@gmail.com>
2026-03-10 22:11:33 +0530

    Getter_setter program

---
+++ Sathyam/src/com/tnsf/packages/packages1/Getter_Setter2.java
@@ -0,0 +1,14 @@
+package com.tnsf.packages.packages1;
+
+public class Getter_Setter2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Getter_setter1 ob = new Getter_setter1();
+		ob.setEid(101);
+		ob.setEname("Hello");
+		System.out.println(ob.getEid());
+		System.out.println(ob.getEname());
+	}
+
+}
---
+++ Sathyam/src/com/tnsf/packages/packages1/Getter_setter1.java
@@ -0,0 +1,19 @@
+package com.tnsf.packages.packages1;
+
+public class Getter_setter1 {
+	private int eid ; 
+	private String ename;
+	
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+}
